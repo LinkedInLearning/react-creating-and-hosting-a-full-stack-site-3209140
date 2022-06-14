0a81f5fdc855a9cffb8628930b9d44e9bf29ae86
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-14 16:19:27 -0400

    Removed reference to not found page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -6,10 +6,6 @@ const ArticlePage = () => {
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
-    if (!article) {
-        return <NotFoundPage />
-    }
-
     return (
         <>
         <h1>{article.title}</h1>
