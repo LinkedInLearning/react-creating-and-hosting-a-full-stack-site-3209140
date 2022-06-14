09fc8bd33a1af46f5e25ec1b4a267f4346631719
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-14 15:22:35 -0400

    Added an articles list to the articles list page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -8,8 +8,8 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
-        {article.content.map(paragraph => (
-            <p>{paragraph}</p>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
         ))}
         </>
     );
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom';
+import articles from './article-content';
+
 const ArticlesListPage = () => {
     return (
-        <h1>This is the articles list page!</h1>
+        <>
+        <h1>Articles</h1>
+        {articles.map(article => (
+            <Link key={article.name} className="article-list-item" to={`/articles/${article.name}`}>
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}...</p>
+            </Link>
+        ))}
+        </>
     );
 }
 
