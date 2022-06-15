745e9df24a7f318b76c5ed1f359ab4fdabd7d70a
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 12:01:36 -0400

    Added URL Param route

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -7,6 +7,11 @@ app.post('/hello', (req, res) => {
     res.send(`Hello ${req.body.name}!`);
 });
 
+app.get('/hello/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
+});
+
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
 });
