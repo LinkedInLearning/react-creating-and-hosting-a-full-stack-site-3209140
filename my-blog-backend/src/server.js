528c04f1d61be5778a22ad11e6145d66e4db19fd
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 12:02:45 -0400

    Added upvote route

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,15 +1,29 @@
 import express from 'express';
 
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+}]
+
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
-});
-
-app.get('/hello/:name', (req, res) => {
+app.put('/api/articles/:name/upvote', (req, res) => {
     const { name } = req.params;
-    res.send(`Hello ${name}!!`);
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
 });
 
 app.listen(8000, () => {
