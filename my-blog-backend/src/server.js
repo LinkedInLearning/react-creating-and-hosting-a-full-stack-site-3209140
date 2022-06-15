7430e552b58dbb3b3e45bcc6f598a2e8850dbd07
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 11:37:41 -0400

    Added post endpoint

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.post('/hello', (req, res) => {
+    res.send(`Hello ${req.body.name}!`);
 });
 
 app.listen(8000, () => {
