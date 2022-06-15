3785087d8571b90717296cf4d34400fcc639314d
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 16:20:31 -0400

    Added an upvote button

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -25,7 +25,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+        res.json(article);
     } else {
         res.send('That article doesn\'t exist');
     }
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -21,6 +21,12 @@ const ArticlePage = () => {
 
     const article = articles.find(article => article.name === articleId);
 
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
+
     if (!article) {
         return <NotFoundPage />
     }
@@ -28,7 +34,10 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
-        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
