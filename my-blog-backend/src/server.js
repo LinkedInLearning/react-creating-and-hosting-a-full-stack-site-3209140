a972a9b6b73828099a3eaccb44419577b9144374
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 12:29:50 -0400

    Added comments endpoint

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -20,9 +23,23 @@ app.put('/api/articles/:name/upvote', (req, res) => {
 
     if (article) {
         article.upvotes += 1;
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
     } else {
         res.send('That article doesn\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
     }
 });
 
