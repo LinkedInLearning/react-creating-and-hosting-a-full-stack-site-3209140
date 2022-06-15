8b29e7719bc7f5ca30e3c1d8ce72a2916337260f
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-15 15:38:48 -0400

    Added state to article page

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 import articles from './article-content';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        setArticleInfo({ upvotes: 3, comments: [] });
+    });
+
     const { articleId } = useParams();
     const article = articles.find(article => article.name === articleId);
 
@@ -13,6 +20,7 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
