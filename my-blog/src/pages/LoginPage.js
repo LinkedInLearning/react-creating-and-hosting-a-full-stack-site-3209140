e1ae4f50bf091f85425dc2d04c4cb5722c659c31
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-16 14:50:21 -0400

    Added login page

--- my-blog/src/pages/LoginPage.js
+++ my-blog/src/pages/LoginPage.js
@@ -1,6 +1,39 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return (
+        <>
         <h1>Log In</h1>
+        {error && <p className="error">{error}</p>}
+        <input
+            placeholder="Your email address"
+            value={email}
+            onChange={e => setEmail(e.target.value)} />
+        <input
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={e => setPassword(e.target.value)} />
+        <button onClick={logIn}>Log In</button>
+        <Link to="/create-account">Don't have an account? Create one here</Link>
+        </>
     );
 }
 
