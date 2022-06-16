ed7e90555d2f7bb8176266932fe855539a93bafa
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-06-16 16:24:27 -0400

    Added route protection to upvote and comment endpoints

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -42,16 +42,33 @@ app.get('/api/articles/:name', async (req, res) => {
     }
 });
 
+app.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
+
 app.put('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-   
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvotes: 1 },
-    });
+    const { uid } = req.user;
+
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.json(article);
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.include(uid);
+   
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },
+                $push: { upvoteIds: uid },
+            });
+        }
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle);
     } else {
         res.send('That article doesn\'t exist');
     }
@@ -59,10 +76,11 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy, text } },
+        $push: { comments: { postedBy: email, text } },
     });
     const article = await db.collection('articles').findOne({ name });
 
