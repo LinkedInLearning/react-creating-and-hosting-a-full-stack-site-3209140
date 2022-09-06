5fb04505fedfd9bab0d0b4391ea6f49ddba15e79
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-09-06 13:51:54 -0400

    Removed database name

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.kz783.mongodb.net/?retryWrites=true&w=majority`);
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@<< REPLACE THIS WITH YOUR OWN DATABASE >>/?retryWrites=true&w=majority`);
     await client.connect();
     db = client.db('react-blog-db');
     cb();
